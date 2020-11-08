c18c89ce916f11743c49f9288f0e34d562012e7a
Josh Cornau <hi@joshcornau.de>
2020-11-08 15:08:04 +0100

    Initial Commit of Backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4da111c6857bd916e59cb5f78ad5f085'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '35e44ce3-7210-4c64-a529-0bed7f9b4f5e'
+};
