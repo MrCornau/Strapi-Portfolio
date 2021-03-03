c1d34ca6b99560dbd8a5221fce1b78e6c0d93e6c
Josh Cornau <hi@joshcornau.de>
2021-03-03 16:45:32 +0100

    fixed

--- config/database.js
+++ config/database.js
@@ -5,15 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
-      options: {
-        ssl: false,
-      },
+      options: {},
     },
   },
 });
